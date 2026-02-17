ee214f5fcc0591be2b9ec793df3d210ee3a88230
Maurits the Programmer <mausite@outlook.com>
2026-02-17 17:37:17 +0100

    added extra features

--- app.js
+++ app.js
@@ -44,10 +44,20 @@ function setTodos() {
         return;
     }
     for (const todo of todos) {
-        html += `<li><input type="text" class="todoNameInp" placeholder="Todo...." value="${todo.title}"><textarea class="todoDesc" placeholder="description">${todo.desc}</textarea>                 <button onclick="addTodo(this)">add</button></li>`;
+        html += `<li><input type="text" class="todoNameInp" placeholder="Todo...." value="${todo.title}"><select name='priority' value="${todo.prior}"><option value='urgent'>urgent</option><option value='not-urgent'>not urgent</option></select ><textarea class="todoDesc" placeholder="description">${todo.desc}</textarea>                 <button onclick="addTodo(this)">add</button></li>`;
     }
-    html += "<li><input type='text' class='todoNameInp' placeholder='Todo....''><textarea class='todoDesc' placeholder='description'></textarea><button onclick='addTodo(this)'>add</button></li>";
+    html += "<li><input type='text' class='todoNameInp' placeholder='Todo....''>        <select name='priority'><option value = 'urgent' > urgent</option><option value='not-urgent'>not urgent</option></select ><textarea class='todoDesc' placeholder='description'></textarea><button onclick='addTodo(this)'>add</button></li > ";
     todoUl.innerHTML = html;
+    let counter = 0;
+    for (const element of document.getElementsByClassName('todoNameInp')) {
+        try {
+            element.nextElementSibling.value = todos[counter].prior;
+            counter++;
+        } catch (error) {
+            element.nextElementSibling.value = "not-urgent";
+        }
+
+    }
     attachListeners();
 }
 function deleteTodo(btn) {
@@ -61,9 +71,11 @@ function deleteTodo(btn) {
 }
 function addTodo(btn) {
     let desc = btn.previousElementSibling;
+    let select = desc.previousElementSibling;
     let values = {
-        "title": desc.previousElementSibling.value,
-        "desc": desc.value
+        "title": select.previousElementSibling.value,
+        "desc": desc.value,
+        "prior": select.value
     };
 
     todos.push(values);
@@ -75,9 +87,12 @@ function attachListeners() {
     for (const inp of document.getElementsByClassName('todoNameInp')) {
         inp.addEventListener('keydown', function (e) {
             if (e.key === "Enter") {
+                let select = inp.nextElementSibling;
+                let desc = select.nextElementSibling;
                 let values = {
                     "title": inp.value,
-                    "desc": inp.nextElementSibling.value
+                    "desc": desc.value,
+                    "prior": select.value
                 };
 
                 todos.push(values);
